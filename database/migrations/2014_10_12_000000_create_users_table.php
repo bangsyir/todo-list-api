14613f84f1b92e16ecc6fbcd53a6b795d056006c
bangsyir <bangsyir@gmail.com>
2021-10-31 15:52:07 +0800

    create table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,8 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->integer('phone');
+            $table->string('phone')->nullable();
+            $table->enum('type', ['FREE', 'PREMIUM'])->default('FREE');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
