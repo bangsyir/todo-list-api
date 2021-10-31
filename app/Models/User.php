ed8011d789a1faee7269cf4df06cca1eaeb71899
bangsyir <bangsyir@gmail.com>
2021-10-31 15:51:48 +0800

    add generate token

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone'
     ];
 
     /**
@@ -41,4 +43,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function generateToken($user) {
+        $this->api_token = $user->createToken('myAppToken')->plainTextToken;
+        $this->save();
+        return $this->api_token;
+    }
 }
