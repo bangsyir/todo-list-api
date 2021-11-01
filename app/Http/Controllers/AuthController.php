abf19289b679ec459fea8d89a5d0ef581400d3d1
bangsyir <bangsyir@gmail.com>
2021-11-01 13:42:14 +0800

    create authentication function

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Hash;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function login(Request $request) {
+
+        $validator = Validator::make($request->all(), [   
+            'email' => 'required|email', 
+            'password' => 'required'
+        ]); 
+        $user = User::where('email', '=', $request->email)->first();
+        $status = "error";
+        $message = "";
+        $data = null;
+        $code = 401;
+        if($validator->fails()) {
+            $errors = $validator->errors();
+            $message = $errors;
+        } else {
+            if($user) {
+                // check if password user input same qith password in database
+                if(Hash::check($request->password, $user->password)) {
+                    // generate token
+                    $user->generateToken($user);
+                    // $user->createToken('myAuthApp')->plainTextToken;
+                    $status = 'success';
+                    $message = 'Login success';
+                    $data = $user->toArray();
+                    $code = 200;
+                }else {
+                    $message = 'Login failed, wrong password';
+                }
+            } else {
+                $message = 'Login failed, wrong password';
+            }
+        }
+
+        return response()->json([
+            'status' => 'login',
+            'message' => $message,
+            'status_code' => $code,
+            'data' => $data
+        ],$code);
+    }
+
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'phone' => ' required',
+            'password' => 'required|string|min:8'
+        ]);
+
+        $status = "error";
+        $message = "";
+        $data = null;
+        $code = 400;
+        if($validator->fails()) {
+            $errors = $validator->errors();
+            $message = $errors;
+        } else {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'phone' => $request->phone,
+                'password' => Hash::make($request->password)
+            ]);
+
+            if($user) {
+                $user->generateToken($user);
+                $status = 'success';
+                $message = 'Register successfully';
+                $data = $user->toArray();
+                $code = 200;
+            } else {
+                $message = 'Register failed';
+            }
+        }
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    public function logout(Request $request) {
+        $user = \Auth::user();
+        
+        if($user) {
+            $user->api_token = null;
+            $user->save();
+        }
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Logout success',
+            'data' => null
+        ], 200);
+    }
+}
