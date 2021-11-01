4b80a8b55cf9ad3fd66541a246f119993ab01efc
bangsyir <bangsyir@gmail.com>
2021-11-02 01:28:43 +0800

    create navbar components

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,14 @@
+<div class="bg-light border border-secondary shadow-sm">
+  <ul class="nav justify-content-center">
+    <li class="nav-item">
+      <a class="nav-link active" href="#">Add</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Reminder</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" type="button" id="logout">Logout</a>
+    </li>
+  </ul>
+
+</div>
