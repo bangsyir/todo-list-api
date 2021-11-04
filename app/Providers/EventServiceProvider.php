5e1cdc99bb28764c93dcd874b8b4af6acc235ce9
bangsyir <bangsyir@gmail.com>
2021-11-04 18:30:38 +0800

    register event and listernet

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -7,6 +7,9 @@ use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
 
+use App\Events\LogProcessed;
+use App\Listeners\SaveLog;
+
 class EventServiceProvider extends ServiceProvider
 {
     /**
@@ -18,6 +21,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        LogProcessed::class => [
+            SaveLog::class,
+        ],
     ];
 
     /**
@@ -27,6 +33,6 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        parent::boot();
     }
 }
