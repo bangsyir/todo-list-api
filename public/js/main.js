7a1663bef9b6bec09600f4788edab994edd5d81a
bangsyir <bangsyir@gmail.com>
2021-11-04 12:29:16 +0800

    update current link location

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,23 @@
-// set idle parameter to localStorage
-// localStorage.setItem('api_token', '')
-// localStorage.setItem('isAuth', 0)
+const current_loc = window.location.href
 
 const logout = document.getElementById('logout')
-
-logout.addEventListener("click",(e) => {
-  e.preventDefault();
-  const token = localStorage.getItem('api_token')
-  console.log(token)
-  fetch('/v1/logout', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
-    }
+if(logout) {
+  logout.addEventListener("click",(e) => {
+    e.preventDefault();
+    const token = localStorage.getItem('api_token')
+    fetch('/v1/logout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .then(response => response.json())
+    .then((result) => {
+      localStorage.removeItem('api_token')
+      localStorage.removeItem('isAuth')
+  
+      window.location.href = "/login";
+    })
   })
-  .then(response => response.json())
-  .then((result) => {
-    localStorage.removeItem('api_token')
-    localStorage.removeItem('isAuth')
-
-    window.location.href = "/login";
-  })
-})
+}
