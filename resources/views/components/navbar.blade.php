67a5c694d271cdbce810f87763fccef9ddbdb859
bangsyir <bangsyir@gmail.com>
2021-11-04 12:30:26 +0800

    update: add modal todo toggle

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-light border border-secondary shadow-sm">
   <ul class="nav justify-content-center">
     <li class="nav-item">
-      <a class="nav-link active" href="#">Add</a>
+      <a class="nav-link active" href="#addModal" data-toggle="modal" data-target="#addModal" id="addTodoBtn">Add</a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#">Reminder</a>
@@ -10,5 +10,4 @@
       <a class="nav-link" type="button" id="logout">Logout</a>
     </li>
   </ul>
-
 </div>
