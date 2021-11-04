543b43c06b03a8f0d4889d300bc2862126de4726
bangsyir <bangsyir@gmail.com>
2021-11-04 12:29:51 +0800

    create: add modal

---
+++ resources/views/components/addModal.blade.php
@@ -0,0 +1,32 @@
+<div class="modal fade" id="addModal" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="addTodoModalLabel">Add Todo</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="alert alert-success d-none" id="addAlertSuccess">
+          Hello
+        </div>
+        <form method="POST" class="todoForm" id="addTodoForm">
+          <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" name="title" id="title" class="form-control" placeholder="input title">
+            <small id="title-error" class="text-danger d-none">error message</small>
+          </div>
+          <div>
+            <label for="description">description</label>
+            <textarea name="description" id="description" class="form-control" placeholder="input description" rows="5"></textarea>
+            <small id="description-error" class="text-danger d-none">error message</small>
+          </div>
+          <div class="float-right pt-2">
+            <button class="btn btn-primary btn-sm" type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
